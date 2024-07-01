5de156a1dd1c036b93e6c67a7f3753d390fff7df
Umer Farooq <umerfarooq@9ostech.com>
2024-07-01 10:24:12 +0500

    changes

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using Api.DTOs.Account;
 using Api.Models;
 using Api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
@@ -24,6 +26,14 @@ namespace Api.Controllers
             _jwtService = jwtService;
             _signInManager = signInManager;
             _userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet("refresh-user-token")]
+        public async Task<ActionResult<UserDto>> RefreshToken()
+        {
+            var user = await _userManager.FindByNameAsync(User.FindFirst(ClaimTypes.Email)?.Value);
+            return CreateApplicationUserDto(user);
         }
 
 
